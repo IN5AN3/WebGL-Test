ee94cce84aee7ae50a5be510621ff9813909c005
IN5AN3 <31247420+IN5AN3@users.noreply.github.com>
2019-01-08 14:14:08 +0200

    Base
    Base

---
+++ Assets/Editor/Build.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEditor.Build.Reporting;
+
+public class Build
+{
+    [MenuItem("Build/Build WebGL")]
+    public static void MyBuild()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Scenes/SampleScene.unity" };
+        buildPlayerOptions.locationPathName = "TempBuild/BuildWebGl";
+        buildPlayerOptions.target = BuildTarget.WebGL;
+        buildPlayerOptions.options = BuildOptions.AutoRunPlayer;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
+    }
+}
